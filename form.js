a6ac074aa45979797d2ca8258346139380860c36
Alex Muñoz <alexm.inf1@gmail.com>
2021-06-06 20:35:16 +0200

    decimo commit

--- form.js
+++ form.js
@@ -8,22 +8,15 @@ const paraFormulario = (e) => {
 $formulario.addEventListener('submit', paraFormulario);
 
 //Se anyaden los videos
+/*
 const addVideo = (e, enlace) =>{
     const video = document.createElement("iframe");
     video.src = enlace;
     video.loading="lazy";
-    video.srcdoc = "<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/Y8Wp3dafaMQ?autoplay=1><img src=https://img.youtube.com/vi/Y8Wp3dafaMQ/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? – Wisecrack Edition'><span>▶</span></a>";
+    //video.srcdoc = "<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/Y8Wp3dafaMQ?autoplay=1><img src=https://img.youtube.com/vi/Y8Wp3dafaMQ/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? – Wisecrack Edition'><span>▶</span></a>";
     e.appendChild(video);
 }
 
-/*
-const primerVideo = document.createElement("iframe");
-const segundoVideo = document.createElement("iframe");
-
-miVideo.src = "https://www.youtube.com/embed/Xtnwt-Jq93E";
-miVideo.loading = "lazy";
-$washoku.appendChild(miVideo);
-*/
 
 window.addEventListener("load", (e) =>{
     const $washoku = document.getElementById("video-washoku");
@@ -31,5 +24,20 @@ window.addEventListener("load", (e) =>{
     addVideo($washoku, "https://www.youtube.com/embed/Xtnwt-Jq93E")
     addVideo($mibu, "https://www.youtube.com/embed/e2WvHxVqif4")
 });
-//addVideo($washoku, primerVideo, "https://www.youtube.com/embed/Xtnwt-Jq93E");
-//addVideo($mibu, segundoVideo, "https://www.youtube.com/embed/e2WvHxVqif4");
+*/
+const addVideo = (e, elemento, enlace) =>{
+    //document.removeChild(e);
+    const video = document.createElement("iframe");
+    video.src = enlace;
+    video.loading="lazy";
+    //video.srcdoc = "<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/Y8Wp3dafaMQ?autoplay=1><img src=https://img.youtube.com/vi/Y8Wp3dafaMQ/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? – Wisecrack Edition'><span>▶</span></a>";
+    e.replaceChild(video, elemento);
+}
+const $washoku = document.getElementById("video-washoku");
+const $mibu = document.getElementById("video-mibu");
+$facade = document.getElementById("image-washoku");
+$washoku.addEventListener("click", (e) =>{
+    //const $washoku = document.getElementById("video-washoku");
+    $facade = document.getElementById("image-washoku");
+    addVideo(e.target, $facade, "https://www.youtube.com/embed/Xtnwt-Jq93E")
+})
