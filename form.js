2eebcf2bf518b9ce45c4ffb3531500cf48e37778
Alex Muñoz <alexm.inf1@gmail.com>
2021-06-11 00:11:12 +0200

    final

--- form.js
+++ form.js
@@ -13,7 +13,6 @@ const addVideo = (e, enlace) =>{
     const video = document.createElement("iframe");
     video.src = enlace;
     video.loading="lazy";
-    //video.srcdoc = "<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/Y8Wp3dafaMQ?autoplay=1><img src=https://img.youtube.com/vi/Y8Wp3dafaMQ/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? – Wisecrack Edition'><span>▶</span></a>";
     e.appendChild(video);
 }
 
@@ -32,7 +31,6 @@ const addVideo = (e, elemento, enlace) =>{
     const video = document.createElement("iframe");
     video.src = enlace;
     console.log(e);
-    //video.srcdoc = "<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/Y8Wp3dafaMQ?autoplay=1><img src=https://img.youtube.com/vi/Y8Wp3dafaMQ/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? – Wisecrack Edition'><span>▶</span></a>";
     elemento.replaceChild(video, e.parentElement);
 }
 const $washoku = document.getElementById("video-washoku");
